7c408cb4ed3322cc116e9f6ccec5b5bdaa91aa1a
jukra <jukka@rautanen.info>
2014-10-27 16:02:25 +0200

    Fixed testing

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,11 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   minifyHTML = require('gulp-minify-html'),
   nodeunit = require('gulp-nodeunit'),
-  eslint = require('gulp-eslint');
+  eslint = require('gulp-eslint'),
+  wait = require('gulp-wait');
 
-gulp.task('default', function() {
-  console.log("Minifying JS, CSS and HTML in static folder");
-  gulp.src('./static/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
-  gulp.src('./static/css/*.css')
-      .pipe(minifyCSS({keepBreaks:true}))
-      .pipe(gulp.dest('./dist/'));
-  var opts = {comments:false,spare:false};
-  gulp.src('./static/html/*.html')
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('default', ['minify-css', 'minify-js', 'minify-html'], function() {
+  console.log("Minified JS, CSS and HTML in static folder");
 });
 
 gulp.task('minify-css', function() {
@@ -38,32 +29,9 @@ gulp.task('minify-html', function() {
     .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('test', function () {
-  console.log("Linting app.js");
-  gulp.src(['app.js'])
-    .pipe(eslint({
-        rules:{
-            'quotes': 0,
-            'strict': 0,
-            'no-unused-vars': 0
-        },
-        env:{
-            node:true
-        }
-    }))
-  .pipe(eslint.format());
-  console.log("Minifying JS, CSS and HTML in static folder");
-  gulp.src('./static/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
-  gulp.src('./static/css/*.css')
-      .pipe(minifyCSS({keepBreaks:true}))
-      .pipe(gulp.dest('./dist/'));
-  var opts = {comments:false,spare:false};
-  gulp.src('./static/html/*.html')
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('test', ['lint', 'minify-css', 'minify-js', 'minify-html'], function () {
   gulp.src('test/*.js')
+        .pipe(wait(1500))
         .pipe(nodeunit({
             reporter: 'junit',
             reporterOptions: {
